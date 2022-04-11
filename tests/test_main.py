fd9b07f8f2fd7293b571eecdaf09cc73959b0e9b
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2022-04-11 23:51:08 +0100

    update tests

---
+++ tests/test_main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.9
+
+""" tests """
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+from zipfile import ZIP_DEFLATED, ZipFile
+
+THISDIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(THISDIR.parent))
+
+from mutablezip import MutableZipFile
+
+
+def test_MutableZipFile():
+	with MutableZipFile(f"{THISDIR}/data/mutable.zip", "a", compression=ZIP_DEFLATED) as zipFile:
+		with zipFile.open("foo.txt", "r") as file:
+			lines = [line.strip() for line in file.readlines()]
+		lines.append(b"new line")
+		zipFile.writestr("foo.txt", b"\n".join(lines))
+	with ZipFile(f"{THISDIR}/data/mutable.zip", "r") as zipFile:
+		assert len(zipFile.namelist()) == 1
+
+
+def test_ZipFile():
+	with ZipFile(f"{THISDIR}/data/immutable.zip", "a", compression=ZIP_DEFLATED) as zipFile:
+		with zipFile.open("foo.txt", "r") as file:
+			lines = [line.strip() for line in file.readlines()]
+		lines.append(b"new line")
+		zipFile.writestr("foo.txt", b"\n".join(lines))
+	with ZipFile(f"{THISDIR}/data/immutable.zip", "r") as zipFile:
+		assert len(zipFile.namelist()) > 1
